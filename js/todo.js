37880cc4d812868eb78377fb719f1a072c531ead
swethai2i <swetha@ideas2it.com>
2019-09-30 10:53:14 +0530

    changed: comment

--- js/todo.js
+++ js/todo.js
@@ -27,6 +27,9 @@ function initialEvents(defaultTask) {
     defaultTask();
 }
 
+/**
+ * It indicates the default task, in which subtasks can be added without choosing any task
+ */
 function defaultTask() {
     var defaultTodo = {taskname: "Tasks", id: "Tasks", subTasks: subTasks = []};
     var id = defaultTodo.id;
@@ -38,6 +41,8 @@ function defaultTask() {
 
 /**
  * To get the present date in particular format
+ * 
+ * @return - it returns the present date
  */
 function formatDate() {
     return Date.now();
@@ -45,7 +50,7 @@ function formatDate() {
 
 /**
  * To get element based on id for each element
- * @param {*} elementId 
+ * @param {*} elementId - it denotes the element id used to get the element
  */
 function getElementById(elementId) {
     return document.getElementById(elementId);
@@ -53,7 +58,7 @@ function getElementById(elementId) {
 
 /**
  * To get element based on classname for each element
- * @param {*} elementClass 
+ * @param {*} elementClass - it indicates the element class used to get the element
  */
 function getElementsByClassName(elementClass) {
     return document.getElementsByClassName(elementClass);
@@ -61,7 +66,7 @@ function getElementsByClassName(elementClass) {
 
 /**
  * Function used to create any new element
- * @param {*} element 
+ * @param {*} element - used to create a partiuclar element
  */
 function createAnyElement(element) {
     return document.createElement(element);
@@ -69,8 +74,8 @@ function createAnyElement(element) {
 
 /**
  * Function used to append child node to parent node
- * @param {*} parentNode 
- * @param {*} childNode 
+ * @param {*} parentNode - it is the parent node where child node will be appended.
+ * @param {*} childNode - it is the child node which will be appended in the parent node.
  */
 function appendChildToParent(parentNode, childNode) {
     parentNode.appendChild(childNode);
@@ -78,7 +83,7 @@ function appendChildToParent(parentNode, childNode) {
 
 /**
  * To create a textnode for any element
- * @param {*} text 
+ * @param {*} text - it denotes the text node to be appended
  */
 function createTextNode(text) {
     return document.createTextNode(text);
@@ -86,9 +91,9 @@ function createTextNode(text) {
 
 /**
  * To set properties and class for any element
- * @param {*} parentElement 
- * @param {*} parentId 
- * @param {*} parentClass 
+ * @param {*} element - denotes the element to which the properties are to be appended
+ * @param {*} elementId - it indicates the id of the element to be set
+ * @param {*} elementClass - indicates the class to be added to the element
  */
 function setAttributeForElement(element, elementId, elementClass) {
     element.setAttribute("id", elementId);
@@ -111,12 +116,12 @@ var hideText = getElementsByClassName("sidenav-item");
 
 /**
  * To open and close the side navigation
- * @param {*} slideToDoContent 
- * @param {*} hideText 
- * @param {*} widthValue 
- * @param {*} marginLeftValue 
- * @param {*} displayStyle 
- * @param {*} roleStatus 
+ * @param {*} slideToDoContent - indicates the element to slide along with content
+ * @param {*} hideText - element which has to hide while toggling
+ * @param {*} widthValue - it is the width of the side navigation
+ * @param {*} marginLeftValue - it is used to slide the content to left margin along with side navigation 
+ * @param {*} displayStyle - it is used for the display property of paricular element
+ * @param {*} roleStatus - it indicates the status of the navigator, which is used for toggling
  */
 function toggleNav(slideToDoContent, hideText, widthValue, marginLeftValue, displayStyle, roleStatus) {
     getElementById("mySidenav").setAttribute("class", widthValue);
@@ -155,11 +160,15 @@ function openLeftSideNav(){
     toggleNav(slideToDoContent, hideText, " sidenav sidenav-width-to-open", "todo-list marginLeft-value-to-slide", " sidenav-item inline-display", "false");
 }
 
-/* To change icon to circle while focus-in and changes to plus icon while focus */
+/**
+ * To change icon to circle while focus-in and changes to plus icon while focus
+ */
 function changeIconToCircle() {
     getElementById("icon").setAttribute("class", "subtask-icon-circle");
 }
-
+/**
+ * To change the icon to plus while focusing out
+ */
 function changeIconToPlus() {
     getElementById("icon").setAttribute("class", "subtask-icon")
 }
@@ -167,7 +176,7 @@ function changeIconToPlus() {
 
 /**
  * To get task input value while enter key was pressed
- * @param {*} event 
+ * @param {*} event - it is the event which is used for adding the task
  */
 function getTaskInput(event) {
     var input = getElementById("new-item");
@@ -185,7 +194,7 @@ function getTaskInput(event) {
 
 /**
  * To find the task name to be unique by checking the task names present
- * @param {*} inputText 
+ * @param {*} inputText - input entered which is used for uniqueness validation
  */
 function findDuplicateTaskName(inputText) {
     var duplicateTask = toDoList.filter(function(task) {
@@ -207,7 +216,7 @@ function findDuplicateTaskName(inputText) {
 
 /**
  * To create new task object
- * @param {*} inputText 
+ * @param {*} inputText - it is the task name, using which the task object was created.
  */
 function createTaskObject (inputText) {
     var todo = {taskname: inputText, id: formatDate(), subTasks: subTasks = []};
@@ -219,9 +228,9 @@ function createTaskObject (inputText) {
 }
 
 /**
- * To add style to each task
- * @param {*} inputText 
- * @param {*} id 
+ * To add style to each task while creating and displaying
+ * @param {*} inputText - it is the input text which is appended on a particular node.
+ * @param {*} id - It is used to add the task id to each task
  */
 function addTaskStyle (inputText, id) {
     var taskList = createAnyElement("div");
@@ -230,7 +239,6 @@ function addTaskStyle (inputText, id) {
     setAttributeForElement(taskIcon, id, "sidenav-list");
     var taskName = createAnyElement("div");
     setAttributeForElement(taskName, id, "sidenav-item inline-display");
-    //taskName.style.display = "inline-block";
     var textNode = createTextNode(inputText);
     appendChildToParent(taskName, textNode);
     appendChildToParent(taskList, taskIcon);
@@ -242,10 +250,9 @@ function addTaskStyle (inputText, id) {
 
 /**
  * To fetch the task name by using click event and displaying the name
- * @param {*} id 
+ * @param {*} id - id which is used to get the target id using click event
  */
 function findTaskUsingId(id) {
-    //console.log("clickkkkkkkkkk");
     getElementById(id).addEventListener("click", function(event) {
        var targetId = event.target.id;
        activeTask = toDoList.find(function(taskEvent) {
@@ -259,16 +266,13 @@ function findTaskUsingId(id) {
 
 /**
  * To display the subtask for each task 
- * @param {*} activeTask 
+ * @param {*} activeTask - it indicates the task which is selected
  */
 function displaySubtasks(activeTask) {
-    //console.log("clickkkkkkkkkk inside display");
     getElementById("list-of-subtask").innerHTML = "";
     getElementById("toggle-right").setAttribute("class", "right-sidenav none-display");
     var showSubtask = activeTask.subTasks;
-    //console.log("currentsubtask-----", showSubtask);
     for(var i = 0; i < showSubtask.length; i++) {
-      // console.log("currentsubtask-----", showSubtask[i].checked);
         if (!showSubtask[i].isDeleted) {
             if (!showSubtask[i].checked) {
                 addSubtaskStyle(showSubtask[i].subtaskName, showSubtask[i].id);
@@ -282,7 +286,7 @@ function displaySubtasks(activeTask) {
 
 /**
  * If new subtask was added using ENTER button
- * @param {*} event 
+ * @param {*} event - it is the keyboard event used to identify if the Enter button is pressed
  */
 function getSubtaskInput(event) {
     var subtaskInput = getElementById("subtask-input");
@@ -298,7 +302,7 @@ function getSubtaskInput(event) {
 
 /**
  * To create subtask object for each task
- * @param {*} subtaskInputText 
+ * @param {*} subtaskInputText - it is the subtask input used to create subtask object
  */
 function createSubtask (subtaskInputText) {
     var subtaskTodo = {id: formatDate(), subtaskName: subtaskInputText, checked:false, isDeleted:false, steps:steps=[]};
@@ -308,9 +312,9 @@ function createSubtask (subtaskInputText) {
 }
 
 /**
- * To add style to each subtask object
- * @param {*} subtaskInputText 
- * @param {*} subtaskId 
+ * To add style to each subtask object while creating and displaying.
+ * @param {*} subtaskInputText - it is the subtask input text
+ * @param {*} subtaskId - it is the subtask id which is added to each element
  */
 function addSubtaskStyle (subtaskInputText, subtaskId) {
     console.log("clickkkkkkkkkk66666666");
@@ -330,33 +334,34 @@ function addSubtaskStyle (subtaskInputText, subtaskId) {
 }
 
 /**
- * 
- * @param {*} subtaskInputText 
- * @param {*} subtaskId 
+ * Method used to display the subtask text and icon by checking the check circle status.
+ * It will be called if the check circle is checked. 
+ * @param {*} subtaskInputText - it indicates the input text of subtask
+ * @param {*} subtaskId - denotes the subtask id to be added
  */
 function checkedSubtaskStyle(subtaskInputText, subtaskId) {
     console.log("clickkkkkkkkkk2322345554");
     var subtaskList = createAnyElement("div");
-   setAttributeForElement(subtaskList, subtaskId, "subtask-container");
-   var subtaskIcon = createAnyElement("div");
-   setAttributeForElement(subtaskIcon, subtaskId, "dynamic-unchecked-subtask-icon");
-   var subtaskName = createAnyElement("div");
-   setAttributeForElement(subtaskName, subtaskId, "subtask-text text-linethrough");
-   //subtaskName.style.textDecoration = 'line-through';
-   var subtasktextNode = createTextNode(subtaskInputText);
-   appendChildToParent(subtaskName, subtasktextNode);
-   appendChildToParent(subtaskList, subtaskIcon);
-   appendChildToParent(subtaskList, subtaskName);
-   var subtaskElement = getElementById("list-of-subtask");
-   appendChildToParent(subtaskElement, subtaskList);
-   findSubtaskUsingId(subtaskId, subtaskIcon, subtaskName);
+    setAttributeForElement(subtaskList, subtaskId, "subtask-container");
+    var subtaskIcon = createAnyElement("div");
+    setAttributeForElement(subtaskIcon, subtaskId, "dynamic-unchecked-subtask-icon");
+    var subtaskName = createAnyElement("div");
+    setAttributeForElement(subtaskName, subtaskId, "subtask-text text-linethrough");
+    //subtaskName.style.textDecoration = 'line-through';
+    var subtasktextNode = createTextNode(subtaskInputText);
+    appendChildToParent(subtaskName, subtasktextNode);
+    appendChildToParent(subtaskList, subtaskIcon);
+    appendChildToParent(subtaskList, subtaskName);
+    var subtaskElement = getElementById("list-of-subtask");
+    appendChildToParent(subtaskElement, subtaskList);
+    findSubtaskUsingId(subtaskId, subtaskIcon, subtaskName);
 }
 
 /**
  * To find the subtask id while clicking the subtask inside content
- * @param {*} subtaskId 
- * @param {*} subtaskIcon 
- * @param {*} subtaskName 
+ * @param {*} subtaskId - it is the subtask id which is used to get the target id
+ * @param {*} subtaskIcon - indicates the subtask icon element
+ * @param {*} subtaskName - inidicates the subtask name
  */
 function findSubtaskUsingId(subtaskId, subtaskIcon, subtaskName) {
     getElementById(subtaskId).addEventListener("click", function(event) {
@@ -380,12 +385,12 @@ function findSubtaskUsingId(subtaskId, subtaskIcon, subtaskName) {
  * To toggle the subtask checkcircle based on the class name clicked by the user.
  * Both checking and unchecking of check circle happens based on the class name
  * and text parameter. It also changes the subtask checked status.
- * @param {*} activeSubtask 
- * @param {*} subtaskIcon 
- * @param {*} subtaskName 
- * @param {*} toggleClass 
- * @param {*} textStyle 
- * @param {*} subtaskStatus 
+ * @param {*} activeSubtask - denotes the subtask which is selected
+ * @param {*} subtaskIcon - subtask icon in which properties are added
+ * @param {*} subtaskName - it is the subtask name in which property class can be added
+ * @param {*} toggleClass - it is property which is used to check and uncheck the icon
+ * @param {*} textStyle - it is property to be added to the sutask name
+ * @param {*} subtaskStatus - indicates the status of subtask
  */
 function toggleContentCheckCircle(activeSubtask, subtaskIcon, subtaskName, toggleClass, textStyle, subtaskStatus) {
    // console.log("======inside checkedsubtask --- if ====");
@@ -400,7 +405,7 @@ function toggleContentCheckCircle(activeSubtask, subtaskIcon, subtaskName, toggl
 
 /**
  * It is used to show the check circle and text based on the subtask checked status
- * @param {*} activeSubtask 
+ * @param {*} activeSubtask - denotes the active subtask object
  */
 function viewSubtask(activeSubtask) {
     if (!activeSubtask.checked) {
@@ -412,9 +417,9 @@ function viewSubtask(activeSubtask) {
 
 /**
  * It is used to change the icon and text based on the class name
- * @param {*} iconClass 
- * @param {*} textClass 
- * @param {*} activeSubtask 
+ * @param {*} iconClass - indicates the property class which is added to icon
+ * @param {*} textClass - denotes the class which is to be added to text
+ * @param {*} activeSubtask - it indicates the selected subtask
  */
 function styleSubtaskCheckcircle(iconClass, textClass, activeSubtask) {
     getElementById("circle-icon").setAttribute("class", iconClass);
@@ -427,7 +432,7 @@ function styleSubtaskCheckcircle(iconClass, textClass, activeSubtask) {
 
 /**
  * To find the subtask id while clicking the steps heading
- * @param {*} event 
+ * @param {*} event - it indicates the click event which is used to toggle the circle
  */
 function toggleCircleIcon(event) {
    // console.log("======inside clicking stepheading====", event.target.className);
@@ -440,10 +445,10 @@ function toggleCircleIcon(event) {
 
 /**
  * To toggle the check circle of steps heading based on class name 
- * @param {*} classNameToSelect 
- * @param {*} classNameToChange 
- * @param {*} textStyle 
- * @param {*} subtaskStatus 
+ * @param {*} classNameToSelect - represents the class name of an element to select
+ * @param {*} classNameToChange - it indicates the property of class to be added
+ * @param {*} textStyle - represents the style added to the text
+ * @param {*} subtaskStatus - denotes the status which is to be set to the subtask
  */
 function toggleStepsHeadingCheckCircle(classNameToSelect, classNameToChange, textStyle, subtaskStatus) {
     //console.log("======inside clicking stepheading --- if ====");
@@ -457,7 +462,7 @@ function toggleStepsHeadingCheckCircle(classNameToSelect, classNameToChange, tex
 
 /**
  * To display the steps for each task
- * @param {*} activeSubtask 
+ * @param {*} activeSubtask - it denotes the active subtask from which the steps are displayed
  */
 function displaySteps(activeSubtask) {
     getElementById("list-of-steps").innerHTML = "";
@@ -474,7 +479,7 @@ function displaySteps(activeSubtask) {
 
 /**
  * If new steps was added using ENTER button
- * @param {*} event 
+ * @param {*} event - it is the event by which identifies the enter key code
  */
 function getStepInput(event) {
     var stepsInput = getElementById("new-steps");
@@ -490,7 +495,7 @@ function getStepInput(event) {
 
 /**
  * To create step object for each subtask
- * @param {*} stepsInputText 
+ * @param {*} stepsInputText - input text of step to create an step object
  */
 function createStep(stepsInputText) {
     var step = {id:formatDate(), checked:false, isDeleted:false, stepsName:stepsInputText};
@@ -501,8 +506,8 @@ function createStep(stepsInputText) {
 
 /**
  * To add style to each step while creating and displaying
- * @param {*} stepsInputText 
- * @param {*} stepId 
+ * @param {*} stepsInputText - it indicates the input text of the step
+ * @param {*} stepId - it is the step id to be added to each element
  */
 function addStepsStyle (stepsInputText, stepId) {
    var stepList = createAnyElement("div");
@@ -522,8 +527,8 @@ function addStepsStyle (stepsInputText, stepId) {
 
 /**
  * To check the step checked status and load the checked class
- * @param {*} stepsInputText 
- * @param {*} stepId 
+ * @param {*} stepsInputText - it indicates the input text of the step
+ * @param {*} stepId - it is the step id to be added to each element
  */
 function checkedStepsStyle (stepsInputText, stepId) {
     var stepList = createAnyElement("div");
@@ -544,9 +549,9 @@ function checkedStepsStyle (stepsInputText, stepId) {
 
 /**
  * To find the step using id while clicking the step
- * @param {*} stepId 
- * @param {*} stepIcon 
- * @param {*} stepName 
+ * @param {*} stepId - step id which is used to find the target id
+ * @param {*} stepIcon - it is the icon of the step
+ * @param {*} stepName - step name of target step
  */
 function findStepUsingId(stepId, stepIcon, stepName) {
     getElementById(stepId).addEventListener("click", function(event) {
@@ -565,9 +570,9 @@ function findStepUsingId(stepId, stepIcon, stepName) {
 
 /**
  * To check the step status and toggle the text and icon
- * @param {*} activeStep 
- * @param {*} stepIcon 
- * @param {*} stepName 
+ * @param {*} activeStep - it is the active step
+ * @param {*} stepIcon - indicates the icon of step to which the class was added
+ * @param {*} stepName - indicates the step name to which properties are added
  */
 function checkedStep(activeStep, stepIcon, stepName) {
     if (!activeStep.checked) {
@@ -583,7 +588,7 @@ function checkedStep(activeStep, stepIcon, stepName) {
 
 /**
  * On double click event, the element
- * @param {*} event 
+ * @param {*} event - it listens to the double click event
  */
 function updateSubtask(event) {
     var stepHeading = getElementById("subtask-heading");
@@ -594,7 +599,7 @@ function updateSubtask(event) {
 
 /**
  * To update the steps heading
- * @param {*} stepHeading 
+ * @param {*} stepHeading - it indicates the step heading element to which key up event is added
  */
 function updateSubtaskHeading(stepHeading) {
     stepHeading.addEventListener("keyup", function(event) {
@@ -612,6 +617,9 @@ function updateSubtaskHeading(stepHeading) {
     });
 }
 
+/**
+ * Method which is used to delete a subtask from the list of task
+ */
 function deleteSubtask() {
     var result = confirm("" + activeSubtask.subtaskName +" will be permanently deleted. You won't be able to undo this action. Do you want to delete ?");
     if (result) {
@@ -625,6 +633,11 @@ function deleteSubtask() {
   }
 }
 
+
+/**
+ * Method used to change the status of steps inside the deleted subtask to true
+ * @param {*} activeSubtask - it indicates the subtask to be deleted
+ */
 function deleteSteps(activeSubtask) {
     var stepsToDelete = activeSubtask.steps;
     for (var i = 0; i < stepsToDelete.length; i++) {
